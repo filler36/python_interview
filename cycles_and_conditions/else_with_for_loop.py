b126d97d9f60e26c3f0f30c8ca5c36e4168476a2
Filip Chkhaidze <filip_chkhaidze@epam.com>
2022-09-26 18:26:50 +0200

    grammar corrections

--- cycles_and_conditions/else_with_for_loop.py
+++ cycles_and_conditions/else_with_for_loop.py
@@ -1,11 +1,11 @@
 for i in range(1, 4):
     print(i)
-else:  # Executed because no break in for loop
+else:  # Will be executed because no break in for loop
     print("No break")
 
 
 for i in range(1, 4):
     print(i)
     break
-else:  # Will not executed because of break in for loop
+else:  # Will not be executed because of break in for loop
     print("No break")
