07a1d292baa9449377605cb9f63a2af9fe462c61
filler36 <47821378+filler36@users.noreply.github.com>
2023-08-11 16:43:11 +0200

    Update atexit_decorator.py

--- decorators/atexit_decorator.py
+++ decorators/atexit_decorator.py
@@ -1,12 +1,27 @@
+import os
+import sys
 import atexit
+
+"""
+Functions that are registered are automatically executed upon interpreter termination.
+Whenever a program is killed by a signal not handled by Python, when os.exit() is called,
+or Python fatal internal error is detected, the functions registered via this module are not executed.
+"""
+
 
 @atexit.register
 def goodbye():
     print('Goodbye!')
 
-@atexit.register
+
 def hello():
     print('Hello!')
 
+
 if __name__ == '__main__':
     hello()
+    # exit()  # goodbye will be executed
+    # sys.exit()  # goodbye will be executed
+    # raise Exception  # goodbye will be executed
+    # os._exit(0)  # goodbye will be executed
+    # os._exit(1)  # goodbye will NOT be executed
