6f252384eeb65b2e90214489d89fd717ab758232
Filip Chkhaidze <filip_chkhaidze@epam.com>
2024-03-20 23:26:14 +0100

    add strings

---
+++ strings/special_characters.py
@@ -0,0 +1,6 @@
+str1 = 'hello\r'  # hello
+str2 = 'hello\ra'  # a
+str3 = 'hello\b'  # hell
+str4 = 'hell\to'  # hell	o
+str5 = 'hell\no'  # hell
+                  # o
---
+++ strings/string_syntax.py
@@ -0,0 +1,13 @@
+str1 = "Hello, Python!"  # str1 == str2 -> True
+str2 = 'Hello, Python!'
+str3 = '''Hello,
+Python!'''  # str1 == str3 -> False
+str4 = """Hello,
+Python!"""  # str3 == str4 -> True
+str5 = 'Hello,' \
+       ' Python!'  # str1 == str5 -> True
+
+str6 = (
+    "Hello, "
+    "Python!"
+)  # str1 == str6 -> True
