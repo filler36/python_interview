fa73fef2b616cd351b7dcf47ab5650b62a9b5ddf
Filip Chkhaidze <filip_chkhaidze@epam.com>
2024-03-28 15:36:24 +0100

    mod

--- async/threading/target_class.py
+++ async/threading/target_class.py
@@ -1,3 +1,4 @@
+import threading
 import time
 from threading import Thread
 
@@ -12,5 +13,10 @@ class Countdown:
             time.sleep(1)
 
 
-t = Thread(target=Countdown(5))
+t = Thread(target=Countdown(5), name='First thread')  # We can set Thread name
+print(t.name)  # First thread will be printed
+print(t.ident)  # Unique identificator of the thread = None -> It will be set after starting
+print(threading.enumerate())  # [<_MainThread(MainThread, started 21468)>]
 t.start()
+print(t.ident)  # Unique identificator of the thread
+print(threading.enumerate())  # [<_MainThread(MainThread, started 35528)>, <Thread(First thread, started 2072)>]
