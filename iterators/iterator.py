b13cc9c4b6752a28c6aaacadbea6a045ff20e0ec
Filip Chkhaidze <filip_chkhaidze@epam.com>
2024-06-11 22:55:07 +0200

    mod

--- iterators/iterator.py
+++ iterators/iterator.py
@@ -1,6 +1,6 @@
 class PowerIterator:
     """ This custom iterator generates numbers in range [0, limit] to the defined power """
-    def __init__(self, power: int = 2, limit: int = 5):
+    def __init__(self, power: int = 2, limit: int = 4):
         self.number = 0
         self.power = power
         self.limit = limit
@@ -16,11 +16,15 @@ class PowerIterator:
         raise StopIteration
 
 
-i = iter(PowerIterator())
+power_iterator = PowerIterator()  # Initialize PowerIterator
+i = iter(PowerIterator())  # get iterator object from power_iterator
 
-print(next(i))
-print(next(i))
-print(next(i))
-print(next(i))
-print(next(i))
+print(next(i))  # 0
+print(next(i))  # 1
+print(next(i))  # 4
+print(next(i))  # 9
+print(next(i))  # 16
 # print(next(i))  # This statement will raise exception StopIteration
+
+for i in power_iterator:  # You can use for loop to iterate through iterable object power_iterator
+    print(i)
