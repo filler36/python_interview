6ad75b0e4b7e804d9a5bb54a7ef658c7e145156c
Filip Chkhaidze <filip_chkhaidze@epam.com>
2024-06-11 22:44:50 +0200

    mod

--- iterators/iterators_or_iterables.py
+++ iterators/iterators_or_iterables.py
@@ -1,12 +1,17 @@
-# Iteration is a general term for taking each item of something, one after another
-# Any time you use a loop, explicit or implicit, to go over a group of items, that is iteration
+"""
+Iteration is a general term for taking each item of something, one after another
+Any time you use a loop, explicit or implicit, to go over a group of items, that is iteration
+"""
 for i in range(5):  # This is the process of iteration
     ...
 
-# An iterable is an object that has an __iter__ method which returns an iterator,
-# or which defines a __getitem__ method that can take sequential indexes starting from zero
-# (and raises an IndexError when the indexes are no longer valid)
-# So an iterable is an object that you can get an iterator from. For example: String object
+"""
+An iterable object is an object that implements __iter__, which is expected to return an iterator object.
+So an iterable is an object that you can get an iterator from. For example: String object
+
+An iterator object implements __next__, which is expected to return the next element of the iterable object that returned it,
+and to raise a StopIteration exception when no more elements are available.
+"""
 
 str1 = 'Hello World!'  # This String is ITERABLE object
 str_iter = iter(str1)  # This iter func will call str1.__iter__ internal method and will return ITERATOR object
