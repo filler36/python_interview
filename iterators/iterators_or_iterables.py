ad12d9fb968d4d2431188e960038a9acc0417f7f
Filip Chkhaidze <filip_chkhaidze@epam.com>
2024-06-12 10:19:56 +0200

    mod

--- iterators/iterators_or_iterables.py
+++ iterators/iterators_or_iterables.py
@@ -6,10 +6,19 @@ for i in range(5):  # This is the process of iteration
     ...
 
 """
-An iterable object is an object that implements __iter__, which is expected to return an iterator object.
-So an iterable is an object that you can get an iterator from. For example: String object
+ITERABLE
+An object capable of returning its members one at a time. Examples of iterables include all sequence types
+(such as list, str, and tuple) and some non-sequence types like dict, file objects, and objects of any classes
+you define with an __iter__() method or with a __getitem__() method that implements sequence semantics.
+Iterables can be used in a for loop and in many other places where a sequence is needed (zip(), map(), …).
+When an iterable object is passed as an argument to the built-in function iter(), it returns an iterator for the object.
+This iterator is good for one pass over the set of values.
+When using iterables, it is usually not necessary to call iter() or deal with iterator objects yourself.
+The for statement does that automatically for you, creating a temporary unnamed variable to hold the iterator for the duration of the loop
+"""
 
-An iterator object implements __next__, which is expected to return the next element of the iterable object that returned it,
+"""
+An ITERATOR object implements __next__, which is expected to return the next element of the iterable object that returned it,
 and to raise a StopIteration exception when no more elements are available.
 """
 
